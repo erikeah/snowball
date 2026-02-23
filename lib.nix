7ebb0b16fda1b126e86da700de331f76f88ca229
Erik Alonso <erikeah@protonmail.com>
2026-02-23 13:53:47 +0000

    feat: patch self

--- lib.nix
+++ lib.nix
@@ -2,6 +2,9 @@
 {
   mkSnowball = { inputs, ... }:
   {
-      roll = args: flake: let mergedInputs = inputs // args.inputs; in flake mergedInputs;
+      roll = args: flake: let
+          mergedInputs = inputs // args.inputs;
+          patchedInputs = mergedInputs // { self.inputs = mergedInputs; };
+          in flake patchedInputs;
   };
 }
